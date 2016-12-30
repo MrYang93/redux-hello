71b9e1bb09730cc35e98ff5745c98bbd2bbdc59b
Peter Wang <happypeter1983@gmail.com>
2016-12-30 10:53:43 +0800

    INCREMENT_LIKES works

--- src/components/PostBody.js
+++ src/components/PostBody.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import store from '../store';
 
 
 class PostBody extends Component {
+  handleClick(){
+    store.dispatch({type: 'INCREMENT_LIKES', index: this.props.postId - 1})
+  }
   render(){
     return(
       <div className="post-body">
-        <div className="likes-num num">
+        <div className="likes-num num" onClick={this.handleClick.bind(this)}>
           { this.props.posts[this.props.postId - 1].likes } 喜欢
         </div>
         <div className="comment-num num">
